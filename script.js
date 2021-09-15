075dd75008a9c63d63113d57ff7233bcd9b7d563
Hamed Taherkhani <hamedtahi7@gmail.com>
2021-09-15 16:47:15 +0430

    Add mouse handler to  script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const collections = document.querySelectorAll(".collection");
+const moviesMatrix = createMatrix(collections);
 
+/* Mouse listener */
+moviesMatrix.forEach((value) => {
+  for (let i = 0; i < value.childElementCount; i++) {
+    value.children[i].addEventListener("mouseover", mouseOverHandler);
+    value.children[i].addEventListener("mouseout", mouseOutHandler);
+  }
+});
+function mouseOverHandler() {
+  let elementStyle = this.style;
+  elementStyle.transform = "scale(1.1)";
+  elementStyle.zIndex = "1";
+}
+
+function mouseOutHandler() {
+  let elementStyle = this.style;
+  elementStyle.transform = "scale(1)";
+  elementStyle.zIndex = "auto";
+}
+/* end */
+
+function createMatrix(collections) {
+  let collectionLength = collections.length,
+    matrix = new Array(collectionLength);
+  for (let i = 0; i < collectionLength; i++) {
+    matrix[i] = collections[i].children[1];
+  }
+  return matrix;
+}
